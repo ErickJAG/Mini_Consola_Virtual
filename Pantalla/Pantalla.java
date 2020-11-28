41e8cf417b9a627c12f3f8338541c5a9990b9934
Erick <ag.erick@hotmail.com>
2020-11-28 15:00:07 -0600

    Pantalla Creada

---
+++ Pantalla/Pantalla.java
@@ -0,0 +1,64 @@
+package App;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+
+public class Pantalla extends JFrame {
+	
+	int boundx=5;
+	int boundy=5;
+	int sizex=10;
+	int sizey=10;
+
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Pantalla frame = new Pantalla();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Pantalla() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 585, 609);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		for (int i = 0; i<51; i++) {
+			for (int j = 0; j<51; j++) {
+				JLabel lblNewLabel = new JLabel((String) null);
+				lblNewLabel.setOpaque(true);
+				lblNewLabel.setBackground(Color.BLUE);
+				lblNewLabel.setBounds(boundx, boundy, sizex, sizey);
+				contentPane.add(lblNewLabel);
+				boundx+=11;
+			}
+		boundx=5;
+		boundy+=11;
+		}
+	}
+}
