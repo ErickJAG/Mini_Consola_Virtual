18a7aac9f9432e1030d120c4f2eec25b3fd783a5
Esteban Granda <180401@estudiantec.cr>
2020-11-29 18:13:57 -0600

    comunicacion por sockets

---
+++ Consola_A/src/App/Consola_A.java
@@ -0,0 +1,43 @@
+package App;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Consola_A {
+	
+	private ServerSocket serverSocket;
+	private Socket acceptSocket;
+	private PrintStream output;
+	private BufferedReader input;
+	private Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[]args) {
+		Consola_A server = new Consola_A();
+		server.run();
+	}
+	
+	public void run () {
+		try {
+			serverSocket = new ServerSocket(7935);
+			acceptSocket = serverSocket.accept();
+			
+			
+			output = new PrintStream(acceptSocket.getOutputStream());
+			input = new BufferedReader(new InputStreamReader(acceptSocket.getInputStream()));
+			
+			while(acceptSocket.isConnected()) {
+			String message = input.readLine();
+			System.out.println(message);
+			
+			String reply = scan.nextLine();
+			output.println("servidor: "+reply);
+			
+			}
+			
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
