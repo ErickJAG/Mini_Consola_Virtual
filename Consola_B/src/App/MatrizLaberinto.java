cb886d90bb3a3f04f57fbb0034e3b6dd00a0da6b
Erick <ag.erick@hotmail.com>
2020-12-02 23:05:29 -0600

    Matrices de juegos creadas

---
+++ Consola_B/src/App/MatrizLaberinto.java
@@ -0,0 +1,13 @@
+package App;
+
+public class MatrizLaberinto{
+	
+	static int [][] matriz = new int [50][50];
+
+public static void main(String[] args) {
+	for (int i = 0; i<50; i++) {
+		for (int j = 0; j<50; j++) {
+			}
+		}
+	}
+}
