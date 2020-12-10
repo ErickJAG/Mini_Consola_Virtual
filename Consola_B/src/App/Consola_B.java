6118bd96381e274271e2fa1cb269b8178e8f555a
Esteban Granda <180401@estudiantec.cr>
2020-12-10 10:46:51 -0600

    logica parcial de pacman

--- Consola_B/src/App/Consola_B.java
+++ Consola_B/src/App/Consola_B.java
@@ -52,4 +52,78 @@ public class Consola_B {
 			e.printStackTrace();
 		}
 	}
+	private void pacman(String comando, int [][] matriz) {
+		if (comando == "DpadU") {
+			for (int i = 0; i <= 50; i = i ++) {
+				for (int j = 0; j <= 50; j = j ++) {
+					if (matriz[i][j] == 8) {
+						if (matriz[i-1][j]!=3) {
+							if (matriz[i-1][j]==6) {
+								contador-=1;
+								if (contador<=0) {
+									System.out.println("ganó");
+								}
+
+							}
+							matriz[i-1][j] = 8;
+							matriz[i][j] = 0;
+							
+						}
+					}
+					}
+				}
+		}else if (comando == "DpadD") {
+			for (int i = 0; i <= 50; i = i ++) {
+				for (int j = 0; j <= 50; j = j ++) {
+					if (matriz[i][j] == 8) {
+						if (matriz[i][j+1]!=3) {
+							if (matriz[i][j+1]==6) {
+								contador-=1;
+								if (contador<=0) {
+									System.out.println("ganó");
+								}
+							}
+							matriz[i][j+1] = 8;
+							matriz[i][j] = 0;
+						}
+					}
+					}
+				}
+		}else if(comando == "DpadL") {
+			for (int i = 0; i <= 50; i = i ++) {
+				for (int j = 0; j <= 50; j = j ++) {
+					if (matriz[i][j] == 8) {
+						if (matriz[i][j-1]!=3) {
+							if (matriz[i][j-1]==6) {
+								contador-=1;
+								if (contador<=0) {
+									System.out.println("ganó");
+								}
+							}
+							matriz[i][j-1] = 8;
+							matriz[i][j] = 0;
+						}
+					}
+					}
+				}
+		}else {
+			for (int i = 0; i <= 50; i = i ++) {
+				for (int j = 0; j <= 50; j = j ++) {
+					if (matriz[i][j] == 8) {
+						if (matriz[i+1][j]!=3) {
+							if (matriz[i+1][j]==6) {
+								contador-=1;
+								if (contador<=0) {
+									System.out.println("ganó");
+								}
+							}
+							matriz[i+1][j] = 8;
+							matriz[i][j] = 0;
+						}
+					}
+					}
+				}
+		}
+			
+	}
 }
